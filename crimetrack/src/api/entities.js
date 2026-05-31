191cf332f307f7a5f11e60dbc1670ad594c89729
andrewhartman1998-cell <andrewhartman1998@gmail.com>
2026-05-31 00:25:58 -0400

    Add CrimeTrack Universal

---
+++ crimetrack/src/api/entities.js
@@ -0,0 +1,7 @@
+import { base44 } from "./base44Client";
+
+export const CrimeReport = base44.entities.CrimeReport;
+export const Suspect = base44.entities.Suspect;
+export const OfficerProfile = base44.entities.OfficerProfile;
+export const Evidence = base44.entities.Evidence;
+export const User = base44.entities.User;
