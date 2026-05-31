8e493e61a8fc47fa43a91502531696bb58e0a52e
andrewhartman1998-cell <andrewhartman1998@gmail.com>
2026-05-30 20:54:10 -0400

    Add SafeMatch dating app

---
+++ safematch/src/api/entities.js
@@ -0,0 +1,7 @@
+import { base44 } from "./base44Client";
+
+export const SafeMatchProfile = base44.entities.SafeMatchProfile;
+export const SafeMatchReport = base44.entities.SafeMatchReport;
+export const SafeMatchApplication = base44.entities.SafeMatchApplication;
+export const SafeMatchMessage = base44.entities.SafeMatchMessage;
+export const User = base44.entities.User;
