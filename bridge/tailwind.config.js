b2dbf33f872fc0c0eff65a5dc55ec3728f3c7ce0
andrewhartman1998-cell <andrewhartman1998@gmail.com>
2026-06-01 07:49:55 -0400

    Add Bridge to Stability app files

---
+++ bridge/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        bridge: {
+          green: '#16a34a',
+          teal: '#0d9488',
+          warm: '#f97316',
+          sky: '#0ea5e9',
+        }
+      }
+    }
+  },
+  plugins: [],
+}
